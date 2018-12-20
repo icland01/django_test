0b31bd82a2a0cc7ba13ecd0f464973cca4d2eb10
icland01 <862912149@qq.com>
2018-12-20 16:21:43 +0800

    a5

---
+++ myblog/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+# Create your views here.
+
+def index(request):
+	articles = models.Article.objects.all()
+	return render(request,'blog/index.html',{'articles':articles})
+
+def article_page(request,article_id):
+	article = models.Article.objects.get(pk=article_id)
+	return render(request,'blog/article.html',{'article':article})
